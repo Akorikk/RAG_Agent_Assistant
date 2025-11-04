de7502f73320705739a18a08b8f49b19767ae099
Abhishek Kori <abhishekkori601@gmail.com>
2025-11-04 16:06:17 +0530

    rag_index

--- rag_index.py
+++ rag_index.py
@@ -1,5 +1,17 @@
 import fitz # this help you to extract the txt from pdf 
 import os 
 
-def 
 
+def extract_text_from_pdf(pdf_path: str):
+    doc = fitz.open(pdf_path)
+    full_text = ""
+    for page in doc:
+        full_text += page.get_text()
+    return full_text    
+
+def build_index_pdf(pdf_path: str, persist_dir: str = "./rag_store"):
+    full_text = extract_text_from_pdf(pdf_path)
+    print(full_text)
+
+if __name__ == "__main__":
+    build_index_pdf(r"C:\Users\abhis\OneDrive\Desktop\RAG_Agent_Assistant\docs\sample_rental_agreement.pdf")
